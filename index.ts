1d0a7d5ad2dc4a793f35485f00311595117bf069
zhln-v <58518039+zhln-v@users.noreply.github.com>
2024-12-24 02:23:07 +0300

    Задача 2 - обновлён файл README.md

--- index.ts
+++ index.ts
@@ -1,1 +1,41 @@
-// empty file
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+// Инициализация dotenv
+dotenv.config();
+
+// Получение порта из переменных окружения
+const PORT = process.env.PORT || 3000;
+
+// Создание приложения Express
+const app = express();
+
+// Middleware для работы с JSON
+app.use(express.json());
+
+// Маршрут GET-запроса
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Это вместо второй лабы' });
+});
+
+// Типизация POST-запроса
+interface PostData {
+  last_name: string;
+  value: number;
+}
+
+// Маршрут POST-запроса
+app.post('/data', (req: Request<{}, {}, PostData>, res: Response) => {
+  const { last_name, value } = req.body;
+
+  if (!last_name || !value) {
+    return res.status(400).json({ error: 'Пожалуйста, укажите фамилию и оценку.' });
+  }
+
+  res.status(200).json({ message: `Привет у ${last_name} стоит ${value}!` });
+});
+
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
