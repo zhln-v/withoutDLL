44933d86b98585ff053fd9c8d9edce30f065e74b
zhln-v <58518039+zhln-v@users.noreply.github.com>
2024-12-24 10:53:01 +0300

    Добавил лабораторную работу с NLog

---
+++ NLog/NLog/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NLog;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLog2Var
+{
+    internal class Program
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                CauseOverflow();
+            }
+            catch (OverflowException ex)
+            {
+                Logger.Error(ex, "Произошла ошибка переполнения при выполнении операции.");
+                Console.WriteLine("Ошибка: Произошла ошибка переполнения. Пожалуйста, попробуйте еще раз.");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Произошла непредвиденная ошибка.");
+                Console.WriteLine("Ошибка: Произошла непредвиденная ошибка. Пожалуйста, попробуйте еще раз.");
+            }
+        }
+
+        private static void CauseOverflow()
+        {
+            // Метод, вызывающий переполнение
+            int number = 1;
+            while (true)
+            {
+                number *= number; // Переполнение
+            }
+        }
+    }
+}
